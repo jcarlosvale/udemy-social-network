13dbf7b00d7d3175274886e39de40e568ebe4852
Joao Vale <jcarlos.vale@gmail.com>
2023-04-10 15:59:55 +0100

    initial push

---
+++ src/main/java/com/learning/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.learning.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public List<String> getAll() {
+        return List.of("Bob", "Ana", "Joe");
+    }
+
+}
